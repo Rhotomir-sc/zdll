175e16a9ce5e38cb58dd8cc418b373a8376218db
Aykan <unluturkaykanefe@gmail.com>
2025-12-03 12:58:01 +0300

    Remove learning log comment from merhaba.cpp
    Removed comment about learning log.

--- 2025/merhaba.cpp
+++ 2025/merhaba.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main() {
-    // Zero Day Learning Log - Day 1
     cout << "System Active: Ready for Cyber Security Training!" << endl;
     cout << "Target: Dual Studium in Germany 🇩🇪" << endl;
     
