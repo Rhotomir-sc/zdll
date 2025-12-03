f661ff0bd131df85b42d3cef1928d599157db4ca
Rhotomir-sc <unluturkaykanefe@gmail.com>
2025-12-03 12:51:48 +0300

    First C++ code added.

---
+++ 2025/merhaba.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Zero Day Learning Log - Day 1
+    cout << "System Active: Ready for Cyber Security Training!" << endl;
+    cout << "Target: Dual Studium in Germany 🇩🇪" << endl;
+    
+    return 0;
+}
